a6954b62483cf74ba7f7f3c91e102e2dc2e3dcb6
seokyung <3skjee@naver.com>
2024-04-11 23:44:41 +0900

    ✅ feat: Next Routing 설정

---
+++ app/about-me/project/page.jsx
@@ -0,0 +1,5 @@
+const ProjectPage = () => {
+	return <h2>Project Page</h2>;
+};
+
+export default ProjectPage;
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
-const Home = () => {
+const Page = () => {
 	return <h1>Home</h1>;
 };
 
-export default Home;
+export default Page;

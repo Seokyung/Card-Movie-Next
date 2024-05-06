336e21b095e40e0cd3f703b1688bfc8bb63e6c49
Seokyung <xuejd17@gmail.com>
2024-05-06 23:01:13 +0900

    ➕ feat: client side rendering (CSR)를 이용한 데이터 fetching

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,11 +1,33 @@
-export const metadata = {
-	title: "Home",
-};
+"use client";
+
+import { useEffect, useState } from "react";
+
+// metadata는 server-side component에서만 사용 가능함
+// export const metadata = {
+// 	title: "Home",
+// };
 
 const Home = () => {
+	const [isLoading, setIsLoading] = useState(true);
+	const [movies, setMovies] = useState([]);
+
+	const getMovies = async () => {
+		const res = await fetch(
+			"https://nomad-movies.nomadcoders.workers.dev/movies"
+		);
+		const json = await res.json();
+		setMovies(json);
+		setIsLoading(false);
+	};
+
+	useEffect(() => {
+		getMovies();
+	}, []);
+
 	return (
 		<div>
 			<h1>Home Page</h1>
+			<div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>
 		</div>
 	);
 };

c402ca8e56a8a4c79b62b9386e3f707f2188d92d
Seokyung <xuejd17@gmail.com>
2024-05-06 23:21:09 +0900

    ⚡️ feat: Server Side Rendering (SSR)을 이용한 데이터 fetching

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,35 +1,34 @@
-"use client";
+import { resolve } from "styled-jsx/css";
 
-import { useEffect, useState } from "react";
+// metadata는 server-side components에서만 사용 가능함
+export const metadata = {
+	title: "Home",
+};
 
-// metadata는 server-side component에서만 사용 가능함
-// export const metadata = {
-// 	title: "Home",
-// };
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-const Home = () => {
-	const [isLoading, setIsLoading] = useState(true);
-	const [movies, setMovies] = useState([]);
-
-	const getMovies = async () => {
-		const res = await fetch(
-			"https://nomad-movies.nomadcoders.workers.dev/movies"
-		);
-		const json = await res.json();
-		setMovies(json);
-		setIsLoading(false);
-	};
-
-	useEffect(() => {
-		getMovies();
-	}, []);
-
+const Home = async () => {
+	const movies = await getMovies();
 	return (
 		<div>
 			<h1>Home Page</h1>
-			<div>{isLoading ? "Loading..." : JSON.stringify(movies)}</div>
+			<div>{JSON.stringify(movies)}</div>
 		</div>
 	);
 };
 
 export default Home;
+
+/**
+ * 영화 목록 가져오기 (SSR)
+ * @returns movies
+ */
+async function getMovies() {
+	// 백엔드에서 데이터를 fetch하기 때문에 loading 상태가 안보이는 것처럼 동작함
+	// await new Promise((resolve) => {
+	// 	setTimeout(resolve, 5000);
+	// });
+	const res = await fetch(URL);
+	const json = await res.json();
+	return json;
+}

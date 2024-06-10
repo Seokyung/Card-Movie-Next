017cea8b4a18b601ffdc6c8303de348757b3b7c9
Seokyung <xuejd17@gmail.com>
2024-06-10 17:19:04 +0900

    💄 feat: MovieInfo 컴포넌트 스타일 적용

--- app/(movies)/movies/[id]/page.jsx
+++ app/(movies)/movies/[id]/page.jsx
@@ -1,10 +1,11 @@
 import { Suspense } from "react";
 import MovieInfo from "../../../../components/movies/MovieInfo";
 import MovieVideos from "../../../../components/movies/MovieVideos";
+import styles from "./movies.module.scss";
 
 const MovieDetails = async ({ params: { id } }) => {
 	return (
-		<div>
+		<div className={styles.movies}>
 			{/* MovieInfo 컴포넌트 (로딩 상태일 경우 fallback이 띄워짐) */}
 			<Suspense fallback={<h1>Loading Movie Info</h1>}>
 				<MovieInfo id={id} />
--- components/movies/MovieInfo.jsx
+++ components/movies/MovieInfo.jsx
@@ -1,21 +1,31 @@
 import Image from "next/image";
 import { API_URL } from "../../app/(home)/page";
+import styles from "./MovieInfo.module.scss";
 
 const MovieInfo = async ({ id }) => {
 	const movie = await getMovie(id);
 	return (
-		<div>
-			<Image
-				src={movie.poster_path}
-				alt={`${movie.title} Poster`}
-				width={350}
-				height={500}
-			/>
-			<div>
-				<h1>{movie.title}</h1>
-				<h3>⭐️ {movie.vote_average.toFixed(2)}</h3>
-				<p>{movie.overview}</p>
-				<a href={movie.homepage} target="_blank">
+		<div className={styles.movieInfo}>
+			<div className={styles.movieInfoPoster}>
+				<Image
+					src={movie.poster_path}
+					alt={`${movie.title} Poster`}
+					width={350}
+					height={500}
+					priority
+				/>
+			</div>
+			<div className={styles.movieInfoContent}>
+				<h1 className={styles.movieTitle}>{movie.title}</h1>
+				<h3 className={styles.movieVoteAverage}>
+					⭐️ {movie.vote_average.toFixed(2)}
+				</h3>
+				<p className={styles.movieOverview}>{movie.overview}</p>
+				<a
+					className={styles.movieHomepage}
+					href={movie.homepage}
+					target="_blank"
+				>
 					HomePage &rarr;
 				</a>
 			</div>
